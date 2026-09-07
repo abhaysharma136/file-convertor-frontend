560017774c7b9c0e92a74f564bd6535df01ce3b7
Abhay Sharma <abhaysharmajr@gmail.com>
2026-09-07 22:26:27 +0530

    added additional analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { captureUtmParams } from "./utils/analytics";
 
-createRoot(document.getElementById('root')!).render(
+captureUtmParams();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
